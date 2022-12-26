bd6fd343e21b07b76df81f9d7cc14ae7222da6b8
Kenk <kenghin_lim@hotmail.com>
2022-12-27 03:41:46 +0800

    feat: Basic Dice game

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,30 +2,16 @@
 	import Counter from './Counter.svelte';
 	import welcome from '$lib/images/svelte-welcome.webp';
 	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import RollButton from './RollButton.svelte';
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>Dice Roll</title>
+	<meta name="description" content="Dice Roll" />
 </svelte:head>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+	<RollButton />
 </section>
 
 <style>
---
+++ frontend/src/routes/RollButton.svelte
@@ -0,0 +1,29 @@
+<script>
+	import Dice from './Dice.svelte';
+
+    let roll = 0;
+  
+    function rollDice() {
+      roll = Math.floor(Math.random() * 6) + 1;
+    }
+</script>
+
+<style>
+    button {
+      font-size: 18px;
+      padding: 10px 20px;
+      border-radius: 5px;
+      background-color: #69E444;
+      border: none;
+    }
+    p {
+      font-size: 24px;
+      font-weight: bold;
+    }
+</style>
+  
+<Dice value={roll}/>
+
+<button on:click={rollDice}>Roll Dice</button>
+
+  
