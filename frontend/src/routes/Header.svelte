c9b5486782c557ece37388124bd51636b6c22de0
Kenk <kenghin_lim@hotmail.com>
2023-01-04 18:42:37 +0800

    feat: Simple wrap feature

--- frontend/src/routes/Header.svelte
+++ frontend/src/routes/Header.svelte
@@ -3,37 +3,29 @@
 	import logo from '$lib/images/LUNAR_ROLL_header.svg';
 	import github from '$lib/images/github.svg';
 
-	import { configureChains, createClient } from '@wagmi/core'
-	import { goerli, mainnet } from '@wagmi/core/chains'
-	import { EthereumClient, modalConnectors, walletConnectProvider } from '@web3modal/ethereum'
-	import { Web3Modal } from '@web3modal/html'
+	import { web3Modal } from './web3';
 
-	// // 1. Define constants
-	const projectId = '8e6b5ffdcbc9794bf9f4a1952578365b'
-	const chains = [mainnet, goerli]
+	web3Modal.setTheme({themeColor: 'blackWhite'});
 
-	// // // 2. Configure wagmi client
-	const { provider } = configureChains(chains, [walletConnectProvider({ projectId })])
-	const wagmiClient = createClient({
-	autoConnect: true,
-	connectors: modalConnectors({ appName: 'web3Modal', chains }),
-	provider
-	})
 
-	// // // 3. Create ethereum and modal clients
-	const ethereumClient = new EthereumClient(wagmiClient, chains)
-	export const web3Modal = new Web3Modal({ projectId }, ethereumClient)
 </script>
 
 <header>
 	<div class="header-logo">
 		<img src={logo} alt="LunarRoll" />
-		<w3m-network-switch></w3m-network-switch>
-		<w3m-core-button></w3m-core-button>
+		<div class="web3modal-container">
+			<w3m-network-switch></w3m-network-switch>
+			<w3m-core-button></w3m-core-button>
+		</div>
 	</div>
 </header>
 
 <style>
+	.web3modal-container {
+		display: flex;
+		align-items: top;
+		justify-content: center;
+	}
 	header {
 		display: flex;
 		align-items: center;
---
+++ frontend/src/routes/wrap/+page.svelte
@@ -0,0 +1,42 @@
+<script>
+	import { ethers } from 'ethers';
+	import abi from '../../lib/contracts/WETH.json';
+	console.log('🚀 | abi', abi);
+
+	import { web3Modal, ethereumClient, client } from '../web3';
+
+	import { prepareWriteContract, writeContract } from '@wagmi/core';
+
+	async function wrapETH() {
+		const config = await prepareWriteContract({
+			address: '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6',
+			abi: abi,
+			functionName: 'deposit',
+			args: [],
+			overrides: {
+				value: ethers.utils.parseEther('0.01')
+			}
+		});
+		const data = await writeContract(config);
+		console.log('wrapETH');
+	}
+</script>
+
+<svelte:head>
+	<title>Wrap ETH</title>
+	<meta name="description" content="Dice Roll" />
+</svelte:head>
+
+<section>
+	<button on:click={wrapETH}>Wrap ETH</button>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		/* flex: 0.6; */
+	}
+</style>
